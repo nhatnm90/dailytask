e952b7ca8a48ca290df252667ac186c9187ec771
nhatnguyen <nhat.nm90@gmail.com>
2020-07-30 22:40:20 +0700

    Temp fix endpoint

--- DailyTasks/ClientApp/src/utils/constant.js
+++ DailyTasks/ClientApp/src/utils/constant.js
@@ -1,4 +1,4 @@
 export const constants = {
-    API_URL1: 'https://dailytaskspublish.azurewebsites.net',
-    API_URL: 'http://localhost:5000'
+    API_URL: 'https://dailytaskspublish.azurewebsites.net',
+    API_URL_1: 'http://localhost:5000'
 };
