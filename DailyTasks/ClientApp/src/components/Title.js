1daa16b100857dbc6d7054d4951aa2b4a10df5d7
nhatnguyen <nhat.nm90@gmail.com>
2020-08-08 19:02:17 +0700

    Adding img logo

--- DailyTasks/ClientApp/src/components/Title.js
+++ DailyTasks/ClientApp/src/components/Title.js
@@ -4,7 +4,8 @@ class Title extends Component {
     render() {
         return (
             <div style={{Align: 'center'}} className="page-header">
-                <h2>JUST DO IT</h2>
+                {/*<h2>JUST DO IT</h2>*/}
+                <img width="200px" src='https://i.imgur.com/ybXU2GD.jpg' alt='8.8'  />
             </div>
         );
     }
